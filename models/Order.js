4aa35488690fbef5179239cdc3f780d57e99348f
StasGit <kutsyukk@gmail.com>
2020-06-11 12:51:35 +0300

    Added new Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,79 +1,39 @@
-import { model, Schema } from 'mongoose';
+const mongoose = require('mongoose');
+const item = require('./item').schema;
 
-const OrderSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: {
-    type: String,
-    required: true,
-  },
-  orderItems: [
+const orderSchema = new mongoose.Schema({
+    
+    status: {
+        type: String,
+        required: true,
+        enum: [
+           "delivered",
+            "pending",
+            "canceled"              
+        ]
+    },
+    user:
     {
-      item: {
-        type: Schema.Types.ObjectId,
-        ref: 'product',
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
+        
+        firstName: String,
+        lastName: String,
+        email: String,
+        phoneNumber: String,
+        address:
+        {
+            country: String,
+            city: String,
+            street: String,
+            buildingNumber: String,
+            appartment: String
+        }
     },
-  ],
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  deliveryAddress: {
-    country: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    street: {
-      type: String,
-      required: true,
-    },
-    buildingNumber: {
-      type: String,
-      required: true,
-    },
-  },
-  deliveryType: {
-    type: String,
-    required: true,
-    enum: ['currier', 'post', 'delivery servise'],
-  },
-  contactPhone: {
-    type: Number,
-    required: true,
-  },
-  paymentMethod: {
-    type: String,
-    required: true,
-    enum: [
-      'credit card',
-      'pay pal',
-      'cash',
-      'google pay',
-      'amazon pay',
-      'apple pay',
-    ],
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ['delivered', 'pending', 'canceled'],
-  },
-});
+    dateOfCreation: Date,
+    dateOfDelivery: Date,
+    items:[item],
+    product: String
+   
+}
+);
 
-const Orders = model('order', OrderSchema);
-
-module.exports = Orders;
+module.exports = mongoose.model('Order', orderSchema)
