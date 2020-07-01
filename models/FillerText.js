16cade67dfff279f412a21d3025571a5c9850ef5
YukiAmeka <anyalo307@gmail.com>
2020-07-01 14:46:49 +0300

    Removed filler models

--- models/FillerText.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Language = require('./Language').schema;
-
-const FillerTextSchema = new mongoose.Schema({
-    name: String,
-    text: [Language]
-});
-
-module.exports = mongoose.model('FillerText', FillerTextSchema);
