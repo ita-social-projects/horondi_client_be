4291b1389a420c4d1db4cd65887285d87964eea6
YukiAmeka <anyalo307@gmail.com>
2020-08-13 19:48:18 +0300

    Refactored patterns seeding code

--- import-data/data/4-patterns/patterns.js
+++ import-data/data/4-patterns/patterns.js
@@ -2,110 +2,29 @@ const { mapToLanguages } = require('../../helpers/languages');
 const { mapToImages } = require('../../helpers/images');
 const { getObjectId, getObjectIds } = require('mongo-seeding');
 
-const ukDescriptionStub = 'Фабричний гобелен із ';
-const enDescriptionStub = 'Manufactured tapestry with ';
+let patterns = [];
 
-const patterns = [
-    {
-        id: getObjectId('pink-wave'),
-        name: mapToLanguages(['Рожева хвилька', 'Pink wave']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням рожевої хвильки'),
-                                    enDescriptionStub.concat('pink wave pattern')]),
-        images: mapToImages('pink-wave'),
+mapToPatterns('pink-wave', ['Рожева хвилька', 'Pink wave'], ['Фабричний гобелен із зображенням рожевої хвильки', 'Manufactured tapestry with pink wave pattern']);
+mapToPatterns('blue-wave', ['Синя хвилька', 'Blue wave'], ['Фабричний гобелен із зображенням синьої хвильки', 'Manufactured tapestry with blue wave pattern']);
+mapToPatterns('arrows', ['Стрілки', 'Arrows'], ['Фабричний гобелен із зображенням стрілок', 'Manufactured tapestry with arrow pattern']);
+mapToPatterns('blue', ['Синій', 'Blue'], ['Фабричний гобелен із синім визерунком', 'Manufactured tapestry with blue pattern']);
+mapToPatterns('black', ['Чорний', 'Black'], ['Фабричний гобелен із чорним визерунком', 'Manufactured tapestry with black pattern']);
+mapToPatterns('red', ['Червоний', 'Red'], ['Фабричний гобелен із червоним визерунком', 'Manufactured tapestry with red pattern']);
+mapToPatterns('embroidery', ['Вишивка', 'Embroidery'], ['Фабричний гобелен із зображенням вишивки', 'Manufactured tapestry with embroidery pattern']);
+mapToPatterns('deers', ['Олені', 'Deers'], ['Фабричний гобелен із зображенням оленів', 'Manufactured tapestry with deer pattern']);
+mapToPatterns('people', ['Люди', 'People'], ['Фабричний гобелен із зображенням людей', 'Manufactured tapestry with people pattern']);
+mapToPatterns('flowers', ['Квіти', 'Flowers'], ['Фабричний гобелен із зображенням квітів', 'Manufactured tapestry with flower pattern']);
+
+function mapToPatterns(code, name, description){
+    patterns.push({
+        id: getObjectId(code),
+        name: mapToLanguages(name),
+        description: mapToLanguages(description),
+        images: mapToImages(code),
         material: 'Cotton',
         handmade: false,
         available: true
-    },
-    {
-        id: getObjectId('blue-wave'),
-        name: mapToLanguages(['Синя хвилька', 'Blue wave']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням синьої хвильки'),
-                                    enDescriptionStub.concat('blue wave pattern')]),
-        images: mapToImages('blue-wave'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('arrows'),
-        name: mapToLanguages(['Стрілки', 'Arrows']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням стрілок'),
-                                    enDescriptionStub.concat('arrow pattern')]),
-        images: mapToImages('arrows'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('blue'),
-        name: mapToLanguages(['Синій', 'Blue']),
-        description: mapToLanguages([ukDescriptionStub.concat('синім визерунком'),
-                                    enDescriptionStub.concat('blue pattern')]),
-        images: mapToImages('blue'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('black'),
-        name: mapToLanguages(['Чорний', 'Black']),
-        description: mapToLanguages([ukDescriptionStub.concat('чорним визерунком'),
-                                    enDescriptionStub.concat('black pattern')]),
-        images: mapToImages('black'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('red'),
-        name: mapToLanguages(['Червоний', 'Red']),
-        description: mapToLanguages([ukDescriptionStub.concat('червоним визерунком'),
-                                    enDescriptionStub.concat('red pattern')]),
-        images: mapToImages('red'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('embroidery'),
-        name: mapToLanguages(['Вишивка', 'Embroidery']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням вишивки'),
-                                    enDescriptionStub.concat('embroidery pattern')]),
-        images: mapToImages('embroidery'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('deers'),
-        name: mapToLanguages(['Олені', 'Deers']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням оленів'),
-                                    enDescriptionStub.concat('deer pattern')]),
-        images: mapToImages('deers'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('people'),
-        name: mapToLanguages(['Люди', 'People']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням людей'),
-                                    enDescriptionStub.concat('people pattern')]),
-        images: mapToImages('people'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    },
-    {
-        id: getObjectId('flowers'),
-        name: mapToLanguages(['Квіти', 'Flowers']),
-        description: mapToLanguages([ukDescriptionStub.concat('зображенням квітів'),
-                                    enDescriptionStub.concat('flower pattern')]),
-        images: mapToImages('flowers'),
-        material: 'Cotton',
-        handmade: false,
-        available: true
-    }
-];
+    })
+}
 
 module.exports = patterns;
