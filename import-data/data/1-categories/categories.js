e278616744536edc3ae01ef08beae6eb4be8f1b2
YukiAmeka <anyalo307@gmail.com>
2020-08-13 13:53:21 +0300

    Refactored seeding code for categories

--- import-data/data/1-categories/categories.js
+++ import-data/data/1-categories/categories.js
@@ -2,87 +2,27 @@ const { mapToLanguages } = require('../../helpers/languages');
 const { mapToImages } = require('../../helpers/images');
 const { getObjectId, getObjectIds } = require('mongo-seeding');
 
-const categories = [
-  {
-    id: getObjectId('main-backpacks'),
-    code: 'main-backpacks',
-    name: mapToLanguages(['Рюкзаки', 'Backpacks']),
-    images: { large: 'https://horondi.blob.core.windows.net/horondi/categories/backpacks.jpg' },
-    subcategories: [
-      getObjectId('sub-backpacks')
-    ],
-    isMain: true,
-    available: true
-  },
-  {
-    id: getObjectId('sub-backpacks'),
-    code: 'sub-backpacks',
-    name: mapToLanguages(['Рюкзаки', 'Backpacks']),
-    images: mapToImages('sub-backpack'),
-    subcategories: [],
-    isMain: false,
-    available: true
-  },
-  {
-    id: getObjectId('main-bags'),
-    code: 'main-bags',
-    name: mapToLanguages(['Сумки', 'Bags']),
-    images: { large: 'https://horondi.blob.core.windows.net/horondi/categories/bags.jpg' },
-    subcategories: [
-      getObjectId('sub-bags'),
-      getObjectId('sub-fanny-packs'),
-    ],
-    isMain: true,
-    available: true
-  },
-  {
-    id: getObjectId('sub-bags'),
-    code: 'sub-bags',
-    name: mapToLanguages(['Сумки', 'Bags']),
-    images: mapToImages('sub-bag'),
-    subcategories: [],
-    isMain: false,
-    available: true
-  },
-  {
-    id: getObjectId('sub-fanny-packs'),
-    code: 'sub-fanny-packs',
-    name: mapToLanguages(['Бананки', 'Fanny packs']),
-    images: mapToImages('sub-fanny-pack'),
-    subcategories: [],
-    isMain: false,
-    available: true
-  },
-  {
-    id: getObjectId('main-accessories'),
-    code: 'main-accessories',
-    name: mapToLanguages(['Аксесуари', 'Accessories']),
-    images: { large: 'https://horondi.blob.core.windows.net/horondi/categories/accessories.jpg' },
-    subcategories: [
-      getObjectId('sub-wallets'),
-      getObjectId('sub-laptop-cases'),
-    ],
-    isMain: true,
-    available: true
-  },
-  {
-    id: getObjectId('sub-wallets'),
-    code: 'sub-wallets',
-    name: mapToLanguages(['Гаманці', 'Wallets']),
-    images: mapToImages('sub-wallet'),
-    subcategories: [],
-    isMain: false,
-    available: true
-  },
-  {
-    id: getObjectId('sub-laptop-cases'),
-    code: 'sub-laptop-cases',
-    name: mapToLanguages(['Чехли для ноутбуків', 'Laptop cases']),
-    images: mapToImages('sub-laptop-case'),
-    subcategories: [],
-    isMain: false,
-    available: false
-  }
-];
+let categories = [];
+
+mapToCategories('main-backpacks', ['Рюкзаки', 'Backpacks'], 'https://horondi.blob.core.windows.net/horondi/categories/backpacks.jpg', ['sub-backpacks']);
+mapToCategories('sub-backpacks', ['Рюкзаки', 'Backpacks'], '', []);
+mapToCategories('main-bags', ['Сумки', 'Bags'], 'https://horondi.blob.core.windows.net/horondi/categories/bags.jpg', ['sub-bags', 'sub-fanny-packs']);
+mapToCategories('sub-bags', ['Сумки', 'Bags'], '', []);
+mapToCategories('sub-fanny-packs', ['Бананки', 'Fanny packs'], '', []);
+mapToCategories('main-accessories', ['Аксесуари', 'Accessories'], 'https://horondi.blob.core.windows.net/horondi/categories/accessories.jpg', ['sub-wallets', 'sub-laptop-cases']);
+mapToCategories('sub-wallets', ['Гаманці', 'Wallets'], '', []);
+mapToCategories('sub-laptop-cases', ['Чехли для ноутбуків', 'Laptop cases'], '', []);
+
+function mapToCategories(code, name, image, subCodes){
+  categories.push({
+    id: getObjectId(code),
+    code: code,
+    name: mapToLanguages(name),
+    images: (image.length === 0) ? mapToImages(code) : { large: image },
+    subcategories: getObjectIds(subCodes),
+    isMain: (subCodes.length === 0) ? false : true,
+    available: (code === 'sub-laptop-cases') ? false : true
+  })
+}
 
 module.exports = categories;
