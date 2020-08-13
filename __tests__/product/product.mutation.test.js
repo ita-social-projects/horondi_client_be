815405f8595fbfe231c8b6f5c3e2ba289398d6a1
Volodymyr Trach <vovka.trach@gmail.com>
2020-08-13 11:40:10 +0300

    added test for update product

--- __tests__/product/product.mutation.test.js
+++ __tests__/product/product.mutation.test.js
@@ -5,140 +5,474 @@ const client = require('../../utils/apollo-test-client');
 require('dotenv').config();
 
 let productId;
+const badProductId = '1a1111da11da1111111a111a';
+const newProduct = {
+  category: 'ddc81f5dbac48c38d0403dd3',
+  subcategory: '688ded7be0c2621f2fb17b05',
+  name: [
+    { lang: 'en', value: 'Very Coool Baggy' },
+    { lang: 'ua', value: 'ДУЖЕ СУПЕРСЬКИЙ Рюкзачечок' },
+  ],
+  description: [
+    { lang: 'en', value: 'Baggy is so cool' },
+    { lang: 'ua', value: 'Рюкзачечок - супер кльовий))' },
+  ],
+  mainMaterial: [
+    {
+      lang: 'uk',
+      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+    },
+    {
+      lang: 'en',
+      value:
+        'Canvas-400G padded with a layer of durable and water-resistant material',
+    },
+  ],
+  innerMaterial: [
+    { lang: 'uk', value: 'Oxford 135' },
+    { lang: 'en', value: 'Oxford 135' },
+  ],
+  pattern: [
+    { lang: 'uk', value: 'Вишивка' },
+    { lang: 'en', value: 'Embroidery' },
+  ],
+  patternImages: {
+    large: 'large-embroidery.jpg',
+    medium: 'medium-embroidery.jpg',
+    small: 'small-embroidery.jpg',
+    thumbnail: 'thumbnail-embroidery.jpg',
+  },
+  strapLengthInCm: 100,
+  closure: [
+    { lang: 'uk', value: 'Фастекс (пластикова защіпка)' },
+    { lang: 'en', value: 'Plastic closure' },
+  ],
+  closureColor: 'black',
+  basePrice: 1550,
+  available: true,
+  isHotItem: false,
+  images: {
+    primary: {
+      large: 'large-primary_15.jpg',
+      medium: 'medium-primary_15.jpg',
+      small: 'small-primary_15.jpg',
+      thumbnail: 'thumbnail-primary_15.jpg',
+    },
+    additional: [
+      {
+        large: 'large-additional_15_1.jpg',
+        medium: 'medium-additional_15_1.jpg',
+        small: 'small-additional_15_1.jpg',
+        thumbnail: 'thumbnail-additional_15_1.jpg',
+      },
+      {
+        large: 'large-additional_15_2.jpg',
+        medium: 'medium-additional_15_2.jpg',
+        small: 'small-additional_15_2.jpg',
+        thumbnail: 'thumbnail-additional_15_2.jpg',
+      },
+      {
+        large: 'large-additional_15_3.jpg',
+        medium: 'medium-additional_15_3.jpg',
+        small: 'small-additional_15_3.jpg',
+        thumbnail: 'thumbnail-additional_15_3.jpg',
+      },
+    ],
+  },
+  colors: [
+    {
+      code: 206,
+      name: [
+        { lang: 'uk', value: 'Золотий' },
+        { lang: 'en', value: 'Golden' },
+      ],
+      images: {
+        large: 'large-golden.jpg',
+        medium: 'medium-golden.jpg',
+        small: 'small-golden.jpg',
+        thumbnail: 'thumbnail-golden.jpg',
+      },
+      available: true,
+      simpleName: [
+        { lang: 'uk', value: 'жовтий' },
+        { lang: 'en', value: 'yellow' },
+      ],
+    },
+  ],
+  options: [
+    {
+      size: '50288e8716e80d9569f64e2e',
+      bottomMaterial: 'dadba32060da96e40847166d',
+      description: [
+        { lang: 'ua', value: 'Тканина Кордура' },
+        { lang: 'en', value: 'Cordura fabric' },
+      ],
+      availableCount: 777,
+      additions: [
+        {
+          available: true,
+          name: [
+            { lang: 'uk', value: 'Кишеня' },
+            { lang: 'en', value: 'Pocket' },
+          ],
+          description: [
+            { lang: 'uk', value: 'Бокова кишенька за бажанням' },
+            { lang: 'en', value: 'Side pocket by request' },
+          ],
+          additionalPrice: 10000000,
+        },
+      ],
+    },
+  ],
+};
+const productForUpdate = {
+  category: 'ddc81f5dbac48c38d0403dd3',
+  subcategory: '688ded7be0c2621f2fb17b05',
+  name: [
+    { lang: 'en', value: 'Bad Baggy' },
+    { lang: 'ua', value: 'Жахливий Рюкзачечок' },
+  ],
+  description: [
+    { lang: 'en', value: 'Baggy is so cool' },
+    { lang: 'ua', value: 'Рюкзачечок - супер кльовий))' },
+  ],
+  mainMaterial: [
+    {
+      lang: 'uk',
+      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+    },
+    {
+      lang: 'en',
+      value:
+        'Canvas-400G padded with a layer of durable and water-resistant material',
+    },
+  ],
+  innerMaterial: [
+    { lang: 'uk', value: 'Oxford 135' },
+    { lang: 'en', value: 'Oxford 135' },
+  ],
+  pattern: [
+    { lang: 'uk', value: 'Вишивка' },
+    { lang: 'en', value: 'Embroidery' },
+  ],
+  patternImages: {
+    large: 'large-embroidery.jpg',
+    medium: 'medium-embroidery.jpg',
+    small: 'small-embroidery.jpg',
+    thumbnail: 'thumbnail-embroidery.jpg',
+  },
+  strapLengthInCm: 100,
+  closure: [
+    { lang: 'uk', value: 'Фастекс (пластикова защіпка)' },
+    { lang: 'en', value: 'Plastic closure' },
+  ],
+  closureColor: 'black',
+  basePrice: 1550,
+  available: true,
+  isHotItem: false,
+  images: {
+    primary: {
+      large: 'large-primary_15.jpg',
+      medium: 'medium-primary_15.jpg',
+      small: 'small-primary_15.jpg',
+      thumbnail: 'thumbnail-primary_15.jpg',
+    },
+    additional: [
+      {
+        large: 'large-additional_15_1.jpg',
+        medium: 'medium-additional_15_1.jpg',
+        small: 'small-additional_15_1.jpg',
+        thumbnail: 'thumbnail-additional_15_1.jpg',
+      },
+      {
+        large: 'large-additional_15_2.jpg',
+        medium: 'medium-additional_15_2.jpg',
+        small: 'small-additional_15_2.jpg',
+        thumbnail: 'thumbnail-additional_15_2.jpg',
+      },
+      {
+        large: 'large-additional_15_3.jpg',
+        medium: 'medium-additional_15_3.jpg',
+        small: 'small-additional_15_3.jpg',
+        thumbnail: 'thumbnail-additional_15_3.jpg',
+      },
+    ],
+  },
+  colors: [
+    {
+      code: 206,
+      name: [
+        { lang: 'uk', value: 'Золотий' },
+        { lang: 'en', value: 'Golden' },
+      ],
+      images: {
+        large: 'large-golden.jpg',
+        medium: 'medium-golden.jpg',
+        small: 'small-golden.jpg',
+        thumbnail: 'thumbnail-golden.jpg',
+      },
+      available: true,
+      simpleName: [
+        { lang: 'uk', value: 'жовтий' },
+        { lang: 'en', value: 'yellow' },
+      ],
+    },
+  ],
+  options: [
+    {
+      size: '50288e8716e80d9569f64e2e',
+      bottomMaterial: 'dadba32060da96e40847166d',
+      description: [
+        { lang: 'ua', value: 'Тканина Кордура' },
+        { lang: 'en', value: 'Cordura fabric' },
+      ],
+      availableCount: 7,
+      additions: [
+        {
+          available: true,
+          name: [
+            { lang: 'uk', value: 'Кишеня' },
+            { lang: 'en', value: 'Pocket' },
+          ],
+          description: [
+            { lang: 'uk', value: 'Бокова кишенька за бажанням' },
+            { lang: 'en', value: 'Side pocket by request' },
+          ],
+          additionalPrice: 100,
+        },
+      ],
+    },
+  ],
+};
+const expectedResult = {
+  __typename: 'Product',
+  available: true,
+  basePrice: 1550,
+  category: {
+    __typename: 'Category',
+    _id: 'ddc81f5dbac48c38d0403dd3',
+  },
+  closureColor: 'black',
+  description: [
+    {
+      __typename: 'Language',
+      value: 'Baggy is so cool',
+    },
+    {
+      __typename: 'Language',
+      value: 'Рюкзачечок - супер кльовий))',
+    },
+  ],
+  innerMaterial: [
+    {
+      __typename: 'Language',
+      value: 'Oxford 135',
+    },
+    {
+      __typename: 'Language',
+      value: 'Oxford 135',
+    },
+  ],
+  isHotItem: false,
+  mainMaterial: [
+    {
+      __typename: 'Language',
+      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+    },
+    {
+      __typename: 'Language',
+      value:
+        'Canvas-400G padded with a layer of durable and water-resistant material',
+    },
+  ],
+  name: [
+    {
+      __typename: 'Language',
+      value: 'Very Coool Baggy',
+    },
+    {
+      __typename: 'Language',
+      value: 'ДУЖЕ СУПЕРСЬКИЙ Рюкзачечок',
+    },
+  ],
+  pattern: [
+    {
+      __typename: 'Language',
+      value: 'Вишивка',
+    },
+    {
+      __typename: 'Language',
+      value: 'Embroidery',
+    },
+  ],
+  purchasedCount: null,
+  rate: null,
+  rateCount: null,
+  strapLengthInCm: 100,
+  subcategory: {
+    __typename: 'Category',
+    _id: '688ded7be0c2621f2fb17b05',
+  },
+};
 
 describe('Product mutations', () => {
-  beforeAll(async () => {
+  test('#1 Should add new product', async () => {
     const createProduct = await client.mutate({
       mutation: gql`
-        mutation {
-          addProduct(
-            product: {
-              category: "ddc81f5dbac48c38d0403dd3"
-              subcategory: "688ded7be0c2621f2fb17b05"
-              name: [
-                { lang: "en", value: "Very Coool Baggy" }
-                { lang: "ua", value: "ДУЖЕ СУПЕРСЬКИЙ Рюкзачечок" }
-              ]
-              description: [
-                { lang: "en", value: "Baggy is so cool" }
-                { lang: "ua", value: "Рюкзачечок - супер кльовий))" }
-              ]
-              mainMaterial: [
-                {
-                  lang: "uk"
-                  value: "Canvas-400G прошита додатковим шаром спеціального матеріалу"
-                }
-                {
-                  lang: "en"
-                  value: "Canvas-400G padded with a layer of durable and water-resistant material"
-                }
-              ]
-              innerMaterial: [
-                { lang: "uk", value: "Oxford 135" }
-                { lang: "en", value: "Oxford 135" }
-              ]
-              pattern: [
-                { lang: "uk", value: "Вишивка" }
-                { lang: "en", value: "Embroidery" }
-              ]
-              patternImages: {
-                large: "large-embroidery.jpg"
-                medium: "medium-embroidery.jpg"
-                small: "small-embroidery.jpg"
-                thumbnail: "thumbnail-embroidery.jpg"
-              }
-              strapLengthInCm: 100
-              closure: [
-                { lang: "uk", value: "Фастекс (пластикова защіпка)" }
-                { lang: "en", value: "Plastic closure" }
-              ]
-              closureColor: "black"
-              basePrice: 1550
-              available: true
-              isHotItem: false
-              images: {
-                primary: {
-                  large: "large-primary_15.jpg"
-                  medium: "medium-primary_15.jpg"
-                  small: "small-primary_15.jpg"
-                  thumbnail: "thumbnail-primary_15.jpg"
-                }
-                additional: [
-                  {
-                    large: "large-additional_15_1.jpg"
-                    medium: "medium-additional_15_1.jpg"
-                    small: "small-additional_15_1.jpg"
-                    thumbnail: "thumbnail-additional_15_1.jpg"
-                  }
-                  {
-                    large: "large-additional_15_2.jpg"
-                    medium: "medium-additional_15_2.jpg"
-                    small: "small-additional_15_2.jpg"
-                    thumbnail: "thumbnail-additional_15_2.jpg"
-                  }
-                  {
-                    large: "large-additional_15_3.jpg"
-                    medium: "medium-additional_15_3.jpg"
-                    small: "small-additional_15_3.jpg"
-                    thumbnail: "thumbnail-additional_15_3.jpg"
-                  }
-                ]
-              }
-              colors: [
-                {
-                  code: 206
-                  name: [
-                    { lang: "uk", value: "Золотий" }
-                    { lang: "en", value: "Golden" }
-                  ]
-                  images: {
-                    large: "large-golden.jpg"
-                    medium: "medium-golden.jpg"
-                    small: "small-golden.jpg"
-                    thumbnail: "thumbnail-golden.jpg"
-                  }
-                  available: true
-                  simpleName: [
-                    { lang: "uk", value: "жовтий" }
-                    { lang: "en", value: "yellow" }
-                  ]
-                }
-              ]
-              options: [
-                {
-                  size: "50288e8716e80d9569f64e2e"
-                  bottomMaterial: "dadba32060da96e40847166d"
-                  description: [
-                    { lang: "ua", value: "Тканина Кордура" }
-                    { lang: "en", value: "Cordura fabric" }
-                  ]
-                  availableCount: 777
-                  additions: [
-                    {
-                      available: true
-                      name: [
-                        { lang: "uk", value: "Кишеня" }
-                        { lang: "en", value: "Pocket" }
-                      ]
-                      description: [
-                        { lang: "uk", value: "Бокова кишенька за бажанням" }
-                        { lang: "en", value: "Side pocket by request" }
-                      ]
-                      additionalPrice: 10000000
-                    }
-                  ]
-                }
-              ]
-            }
-          ) {
+        mutation($product: ProductInput!) {
+          addProduct(product: $product) {
             ... on Product {
               _id
             }
           }
         }
       `,
+      variables: { product: newProduct },
     });
     productId = createProduct.data.addProduct._id;
+    const getProduct = await client.query({
+      query: gql`
+        query($id: ID!) {
+          getProductById(id: $id) {
+            ... on Product {
+              _id
+              category {
+                _id
+              }
+              subcategory {
+                _id
+              }
+              name {
+                value
+              }
+              description {
+                value
+              }
+              mainMaterial {
+                value
+              }
+              innerMaterial {
+                value
+              }
+              strapLengthInCm
+              pattern {
+                value
+              }
+              closureColor
+              basePrice
+              available
+              isHotItem
+              purchasedCount
+              rate
+              rateCount
+            }
+            ... on Error {
+              statusCode
+              message
+            }
+          }
+        }
+      `,
+      variables: { id: productId },
+    });
+    const receivedProduct = getProduct.data.getProductById;
+    expect(receivedProduct).toMatchSnapshot();
+    expect(receivedProduct).toBeDefined();
+    expect(receivedProduct).toHaveProperty('name', [
+      { __typename: 'Language', value: 'Very Coool Baggy' },
+      { __typename: 'Language', value: 'ДУЖЕ СУПЕРСЬКИЙ Рюкзачечок' },
+    ]);
+    expect(receivedProduct).toHaveProperty('description', [
+      { __typename: 'Language', value: 'Baggy is so cool' },
+      { __typename: 'Language', value: 'Рюкзачечок - супер кльовий))' },
+    ]);
+    expect(receivedProduct).toHaveProperty('category', {
+      __typename: 'Category',
+      _id: 'ddc81f5dbac48c38d0403dd3',
+    });
+    expect(receivedProduct).toHaveProperty('subcategory', {
+      __typename: 'Category',
+      _id: '688ded7be0c2621f2fb17b05',
+    });
+    expect(receivedProduct).toHaveProperty('mainMaterial', [
+      {
+        __typename: 'Language',
+        value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+      },
+      {
+        __typename: 'Language',
+        value:
+          'Canvas-400G padded with a layer of durable and water-resistant material',
+      },
+    ]);
+    expect(receivedProduct).toHaveProperty('innerMaterial', [
+      {
+        __typename: 'Language',
+        value: 'Oxford 135',
+      },
+      {
+        __typename: 'Language',
+        value: 'Oxford 135',
+      },
+    ]);
+    expect(receivedProduct).toHaveProperty('strapLengthInCm', 100);
+    expect(receivedProduct).toHaveProperty('pattern', [
+      {
+        __typename: 'Language',
+        value: 'Вишивка',
+      },
+      {
+        __typename: 'Language',
+        value: 'Embroidery',
+      },
+    ]);
+    expect(receivedProduct).toHaveProperty('closureColor', 'black');
+    expect(receivedProduct).toHaveProperty('basePrice', 1550);
+    expect(receivedProduct).toHaveProperty('available', true);
+    expect(receivedProduct).toHaveProperty('isHotItem', false);
+    expect(receivedProduct).toHaveProperty('purchasedCount', null);
+    expect(receivedProduct).toHaveProperty('rate', null);
+    expect(receivedProduct).toHaveProperty('rateCount', null);
+  });
+
+  test('#2 Should update new product', async () => {
+    const updateProduct = await client.mutate({
+      mutation: gql`
+        mutation($product: ProductInput!, $id: ID!) {
+          updateProduct(id: $id, product: $product) {
+            ... on Product {
+              _id
+              name {
+                lang
+                value
+              }
+            }
+            ... on Error {
+              statusCode
+              message
+            }
+          }
+        }
+      `,
+      variables: {
+        product: productForUpdate,
+        id: productId,
+      },
+    });
+    const productAfterUpdate = updateProduct.data.updateProduct;
+    expect(productAfterUpdate).toBeDefined();
+    expect(productAfterUpdate).toHaveProperty('name', [
+      {
+        __typename: 'Language',
+        lang: 'en',
+        value: 'Bad Baggy',
+      },
+      {
+        __typename: 'Language',
+        lang: 'ua',
+        value: 'Жахливий Рюкзачечок',
+      },
+    ]);
   });
 
   afterAll(async () => {
