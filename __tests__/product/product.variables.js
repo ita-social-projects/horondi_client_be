add17420abbedbfb5b62c955d4409b0cfef1a013
Volodymyr Trach <vovka.trach@gmail.com>
2020-08-14 10:06:19 +0300

    tried to take out dublicates

--- __tests__/product/product.variables.js
+++ __tests__/product/product.variables.js
@@ -126,26 +126,25 @@ const productForUpdate = {
     { lang: 'ua', value: 'Жахливий Рюкзачечок' },
   ],
   description: [
-    { lang: 'en', value: 'Baggy is so cool' },
-    { lang: 'ua', value: 'Рюкзачечок - супер кльовий))' },
+    { lang: 'en', value: 'Baggy is so bad' },
+    { lang: 'ua', value: 'Рюкзачечок - не добрий))' },
   ],
   mainMaterial: [
     {
       lang: 'uk',
-      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+      value: 'Canvas-400QQ прошита без спеціального матеріалу',
     },
     {
       lang: 'en',
-      value:
-        'Canvas-400G padded with a layer of durable and water-resistant material',
+      value: 'Canvas-400G padded without a layer water-resistant material',
     },
   ],
   innerMaterial: [
-    { lang: 'uk', value: 'Oxford 135' },
-    { lang: 'en', value: 'Oxford 135' },
+    { lang: 'uk', value: 'Oxford 185' },
+    { lang: 'en', value: 'Oxford 185' },
   ],
   pattern: [
-    { lang: 'uk', value: 'Вишивка' },
+    { lang: 'uk', value: 'Вишивочка' },
     { lang: 'en', value: 'Embroidery' },
   ],
   patternImages: {
@@ -159,24 +158,18 @@ const productForUpdate = {
     { lang: 'uk', value: 'Фастекс (пластикова защіпка)' },
     { lang: 'en', value: 'Plastic closure' },
   ],
-  closureColor: 'black',
-  basePrice: 1550,
-  available: true,
-  isHotItem: false,
+  closureColor: 'white',
+  basePrice: 7777,
+  available: false,
+  isHotItem: true,
   images: {
     primary: {
-      large: 'large-primary_15.jpg',
-      medium: 'medium-primary_15.jpg',
-      small: 'small-primary_15.jpg',
-      thumbnail: 'thumbnail-primary_15.jpg',
+      large: 'large-primary_15_1.jpg',
+      medium: 'medium-primary_15_1.jpg',
+      small: 'small-primary_15_1.jpg',
+      thumbnail: 'thumbnail-primary_15_1.jpg',
     },
     additional: [
-      {
-        large: 'large-additional_15_1.jpg',
-        medium: 'medium-additional_15_1.jpg',
-        small: 'small-additional_15_1.jpg',
-        thumbnail: 'thumbnail-additional_15_1.jpg',
-      },
       {
         large: 'large-additional_15_2.jpg',
         medium: 'medium-additional_15_2.jpg',
@@ -184,10 +177,16 @@ const productForUpdate = {
         thumbnail: 'thumbnail-additional_15_2.jpg',
       },
       {
-        large: 'large-additional_15_3.jpg',
-        medium: 'medium-additional_15_3.jpg',
-        small: 'small-additional_15_3.jpg',
-        thumbnail: 'thumbnail-additional_15_3.jpg',
+        large: 'large-additional_15.jpg',
+        medium: 'medium-additional_15.jpg',
+        small: 'small-additional_15.jpg',
+        thumbnail: 'thumbnail-additional_15.jpg',
+      },
+      {
+        large: 'large-additional_15_2.jpg',
+        medium: 'medium-additional_15_2.jpg',
+        small: 'small-additional_15_2.jpg',
+        thumbnail: 'thumbnail-additional_15_2.jpg',
       },
     ],
   },
@@ -195,8 +194,8 @@ const productForUpdate = {
     {
       code: 206,
       name: [
-        { lang: 'uk', value: 'Золотий' },
-        { lang: 'en', value: 'Golden' },
+        { lang: 'uk', value: 'Златий' },
+        { lang: 'en', value: 'Goldy' },
       ],
       images: {
         large: 'large-golden.jpg',
@@ -219,7 +218,7 @@ const productForUpdate = {
         { lang: 'ua', value: 'Тканина Кордура' },
         { lang: 'en', value: 'Cordura fabric' },
       ],
-      availableCount: 7,
+      availableCount: 50,
       additions: [
         {
           available: true,
@@ -307,6 +306,7 @@ const expectedResult = {
     _id: '688ded7be0c2621f2fb17b05',
   },
 };
+
 const sameNameForUpdate = {
   category: 'ddc81f5dbac48c38d0403dd3',
   subcategory: '688ded7be0c2621f2fb17b05',
@@ -321,12 +321,12 @@ const sameNameForUpdate = {
   mainMaterial: [
     {
       lang: 'uk',
-      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+      value: 'Canvas-400B прошита додатковим шаром спеціального матеріалу',
     },
     {
       lang: 'en',
       value:
-        'Canvas-400G padded with a layer of durable and water-resistant material',
+        'Canvas-400B padded with a layer of durable and water-resistant material',
     },
   ],
   innerMaterial: [
