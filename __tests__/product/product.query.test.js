cd4d0278f30b9a17db7527d1d109d26a7dc4f7e1
Volodymyr Trach <vovka.trach@gmail.com>
2020-08-19 13:20:54 +0300

    deleted variables for tests folder and moved it to tests

--- __tests__/product/product.query.test.js
+++ __tests__/product/product.query.test.js
@@ -1,13 +1,136 @@
 /* eslint-disable no-undef */
 const { gql } = require('apollo-boost');
 const client = require('../../utils/apollo-test-client');
-const {
-  badProductId,
-  newProduct,
-} = require('../../tests-variables/product.variables');
 require('dotenv').config();
 
 let productId;
+const badProductId = '1a1111da11da1111111a111a';
+
+const newProduct = {
+  category: 'ddc81f5dbac48c38d0403dd3',
+  subcategory: '688ded7be0c2621f2fb17b05',
+  name: [
+    { lang: 'en', value: 'Very Coool Baggy' },
+    { lang: 'ua', value: 'ДУЖЕ СУПЕРСЬКИЙ Рюкзачечок' },
+  ],
+  description: [
+    { lang: 'en', value: 'Baggy is so cool' },
+    { lang: 'ua', value: 'Рюкзачечок - супер кльовий))' },
+  ],
+  mainMaterial: [
+    {
+      lang: 'uk',
+      value: 'Canvas-400G прошита додатковим шаром спеціального матеріалу',
+    },
+    {
+      lang: 'en',
+      value:
+        'Canvas-400G padded with a layer of durable and water-resistant material',
+    },
+  ],
+  innerMaterial: [
+    { lang: 'uk', value: 'Oxford 135' },
+    { lang: 'en', value: 'Oxford 135' },
+  ],
+  pattern: [
+    { lang: 'uk', value: 'Вишивка' },
+    { lang: 'en', value: 'Embroidery' },
+  ],
+  patternImages: {
+    large: 'large-embroidery.jpg',
+    medium: 'medium-embroidery.jpg',
+    small: 'small-embroidery.jpg',
+    thumbnail: 'thumbnail-embroidery.jpg',
+  },
+  strapLengthInCm: 100,
+  closure: [
+    { lang: 'uk', value: 'Фастекс (пластикова защіпка)' },
+    { lang: 'en', value: 'Plastic closure' },
+  ],
+  closureColor: 'black',
+  basePrice: [
+    { currency: 'UAH', value: 145000 },
+    { currency: 'USD', value: 5229 },
+  ],
+  available: true,
+  isHotItem: false,
+  images: {
+    primary: {
+      large: 'large-primary_15.jpg',
+      medium: 'medium-primary_15.jpg',
+      small: 'small-primary_15.jpg',
+      thumbnail: 'thumbnail-primary_15.jpg',
+    },
+    additional: [
+      {
+        large: 'large-additional_15_1.jpg',
+        medium: 'medium-additional_15_1.jpg',
+        small: 'small-additional_15_1.jpg',
+        thumbnail: 'thumbnail-additional_15_1.jpg',
+      },
+      {
+        large: 'large-additional_15_2.jpg',
+        medium: 'medium-additional_15_2.jpg',
+        small: 'small-additional_15_2.jpg',
+        thumbnail: 'thumbnail-additional_15_2.jpg',
+      },
+      {
+        large: 'large-additional_15_3.jpg',
+        medium: 'medium-additional_15_3.jpg',
+        small: 'small-additional_15_3.jpg',
+        thumbnail: 'thumbnail-additional_15_3.jpg',
+      },
+    ],
+  },
+  colors: [
+    {
+      code: 206,
+      name: [
+        { lang: 'uk', value: 'Золотий' },
+        { lang: 'en', value: 'Golden' },
+      ],
+      images: {
+        large: 'large-golden.jpg',
+        medium: 'medium-golden.jpg',
+        small: 'small-golden.jpg',
+        thumbnail: 'thumbnail-golden.jpg',
+      },
+      available: true,
+      simpleName: [
+        { lang: 'uk', value: 'жовтий' },
+        { lang: 'en', value: 'yellow' },
+      ],
+    },
+  ],
+  options: [
+    {
+      size: '50288e8716e80d9569f64e2e',
+      bottomMaterial: 'dadba32060da96e40847166d',
+      description: [
+        { lang: 'ua', value: 'Тканина Кордура' },
+        { lang: 'en', value: 'Cordura fabric' },
+      ],
+      availableCount: 777,
+      additions: [
+        {
+          available: true,
+          name: [
+            { lang: 'uk', value: 'Кишеня' },
+            { lang: 'en', value: 'Pocket' },
+          ],
+          description: [
+            { lang: 'uk', value: 'Бокова кишенька за бажанням' },
+            { lang: 'en', value: 'Side pocket by request' },
+          ],
+          additionalPrice: [
+            { currency: 'UAH', value: 145000 },
+            { currency: 'USD', value: 5229 },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 describe('Product queries', () => {
   beforeAll(async () => {
