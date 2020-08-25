305e7b1c8bd6d6efaa1a4734e505675feadda293
39312657 <tacjka34@gmail.com>
2020-08-25 16:24:50 +0300

    Test server run

--- server.js
+++ server.js
@@ -4,9 +4,22 @@ const resolvers = require('./resolvers');
 const connectDB = require('./config/db');
 const userService = require('./modules/user/user.service');
 const verifyUser = require('./utils/verify-user');
+const http = require('http')
 
 connectDB();
 require('dotenv').config();
+
+if(process.env.NODE_ENV === 'test') {
+  const HEALTH_PORT = process.env.HEALTH_PORT || 6000;
+
+  const app = http.createServer((req, res) => {
+    res.end('Health page!');
+  });
+    
+  app.listen(HEALTH_PORT, () => {
+    console.log('Server Health started');
+  });
+}
 
 const server = new ApolloServer({
   typeDefs,
@@ -24,5 +37,8 @@ const server = new ApolloServer({
   introspection: true,
   cors: { origin: '*' },
 });
+
 const PORT = process.env.PORT || 5000;
+
+
 server.listen(PORT, () => console.log('apollo server started, port', PORT));
