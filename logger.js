f077ab65c6ea6eb7bfdaa4ec6ca841b7bfaf3fde
bandvov <bandvov@gmail.com>
2020-09-05 20:03:21 +0300

    added logger

---
+++ logger.js
@@ -0,0 +1,28 @@
+const winston = require('winston');
+require('dotenv').config();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const { ApolloServer, AuthenticationError, makeExecutableSchema } = require('apollo-server-express');
+const {
+  ApolloServer,
+  AuthenticationError,
+  makeExecutableSchema,
+} = require('apollo-server-express');
 const { applyMiddleware } = require('graphql-middleware');
-const express = require('express')
+const express = require('express');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const connectDB = require('./config/db');
 const userService = require('./modules/user/user.service');
 const verifyUser = require('./utils/verify-user');
 const permissions = require('./permissions');
+const logger = require('./logger');
 
 connectDB();
 require('dotenv').config();
 
-
 const schema = applyMiddleware(
   makeExecutableSchema({ typeDefs, resolvers }),
-  permissions,
+  permissions
 );
 
 const server = new ApolloServer({
   schema,
   context: async ({ req }) => {
     const { token } = req.headers || '';
+
+    logger.log({
+      level: 'info',
+      message: `method: ${req.method}/baseUrl: ${req.baseUrl}/date:${
+        req.fresh
+      }/ request headers: ${JSON.stringify(req.headers)}/body: ${JSON.stringify(
+        req.body
+      )}`,
+    });
     if (token) {
       const user = verifyUser(token);
       if (!user) throw new AuthenticationError('Invalid authorization token');
+      logger.error({ level: 'error', message: 'Invalid authorization token' });
       return {
         user: await userService.getUserByFieldOrThrow('email', user.email),
       };
@@ -42,5 +56,7 @@ app.get('/health', (req, res) => res.send('Health page!'));
 server.applyMiddleware({ app });
 
 app.listen(PORT, () => {
-  console.log('apollo server started, port', PORT, `,Graphql path: ${server.graphqlPath}`);
+  logger.info(
+    `apollo server started, port: ${PORT}, Graphql path: ${server.graphqlPath}`
+  );
 });
