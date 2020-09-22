01b65d24ede37af2f35f90902cb692258c8e9961
Dmytro Didukh <jeriko.outback@gmail.com>
2020-09-22 19:38:42 +0300

    "update business" text with images: -delete no needed -upload new

--- modules/business-text/business-text.service.js
+++ modules/business-text/business-text.service.js
@@ -5,6 +5,7 @@ const {
   BUSINESS_TEXT_WITH_THIS_CODE_ALREADY_EXIST,
 } = require('../../error-messages/business-text.messages');
 const { uploadFiles, deleteFiles } = require('../upload/upload.service');
+require('dotenv').config();
 
 class BusinessTextService {
   async getAllBusinessTexts() {
@@ -38,6 +39,20 @@ class BusinessTextService {
       };
     }
 
+    const oldPage = await this.getBusinessTextById(id);
+    const oldImages = this.imageFinder(oldPage);
+
+    const updatedPage = await this.pageImageReplaceHandler(businessText, files);
+    const newImages = this.imageFinder(updatedPage);
+
+    const imagesToDelete = oldImages.filter(
+      img => newImages.findIndex(newImg => newImg === img) === -1
+    );
+
+    if (imagesToDelete.length) {
+      await this.deleteNoNeededImages(imagesToDelete);
+    }
+
     const text = await BusinessText.findByIdAndUpdate(id, businessText, {
       new: true,
     });
@@ -52,37 +67,13 @@ class BusinessTextService {
       throw new Error(BUSINESS_TEXT_ALREADY_EXIST);
     }
 
+    let newPage = '';
+
     if (files.length) {
-      const fileNames = files.map(({ file }) => file.filename);
-      console.log(fileNames);
-
-      const ukText = businessText.text[0].value;
-      const enText = businessText.text[1].value;
-
-      const uploadResult = await uploadFiles(files);
-      const imagesResults = await Promise.allSettled(uploadResult);
-
-      console.log(imagesResults[0].value);
-
-      const updatedText = [ukText, enText].map(text => {
-        let newText = '';
-        fileNames.forEach((name, i) => {
-          newText = text.replace(
-            new RegExp(`src=""(?= alt="${name}")`, 'g'),
-            `src="${imagesResults[i].value.prefixUrl}${imagesResults[i].value.fileNames.small}"`
-          );
-        });
-
-        return newText;
-      });
-
-      businessText.text[0].value = updatedText[0];
-      businessText.text[1].value = updatedText[1];
-
-      console.log(businessText.text);
-
-      return new BusinessText(businessText).save();
+      newPage = await this.pageImageReplaceHandler(businessText, files);
     }
+
+    return new BusinessText(newPage || businessText).save();
   }
 
   async deleteBusinessText(id) {
@@ -95,6 +86,69 @@ class BusinessTextService {
 
   async checkBusinessTextExistByCode(data) {
     return await BusinessText.find({ code: data.code });
+  }
+
+  async pageImageReplaceHandler(page, files) {
+    const fileNames = files.map(({ file }) => file.filename);
+
+    const uploadResult = await uploadFiles(files);
+    const imagesResults = await Promise.allSettled(uploadResult);
+
+    let newUkText = '';
+    let newEnText = '';
+
+    fileNames.forEach((name, i) => {
+      const regExp = new RegExp(`src=""(?=.*alt="${name}")`, 'g');
+      const replacer = `src="${imagesResults[i].value.prefixUrl}${imagesResults[i].value.fileNames.small}"`;
+
+      newUkText = newUkText
+        ? newUkText.replace(regExp, replacer)
+        : page.text[0].value.replace(regExp, replacer);
+      newEnText = newEnText
+        ? newEnText.replace(regExp, replacer)
+        : page.text[1].value.replace(regExp, replacer);
+    });
+
+    //      ********* DIFFERENT VARIANT **************
+    // const updatedTexts = [page.text[0].value, page.text[1].value].map((_newText) => {
+    //   fileNames.forEach((name, i) => {
+    //     const regExp = new RegExp(`src=""(?=.*alt="${name}")`, 'g');
+    //     const replacer = `src="${imagesResults[i].value.prefixUrl}${imagesResults[i].value.fileNames.small}"`;
+    //
+    //     // eslint-disable-next-line no-param-reassign
+    //     _newText = _newText.replace(regExp, replacer)
+    //   })
+    //   return _newText
+    // })
+
+    page.text[0].value = newUkText || page.text[0].value;
+    page.text[1].value = newEnText || page.text[1].value;
+
+    return page;
+  }
+
+  async deleteNoNeededImages(images) {
+    const regExp = new RegExp(
+      `(?<=src="${process.env.IMAGE_LINK}[a-z]+_).*?(?=")`
+    );
+    const valuesToDelete = images
+      .flatMap(img => img.match(regExp))
+      .flatMap(id => [
+        `large_${id}`,
+        `medium_${id}`,
+        `small_${id}`,
+        `thumbnail_${id}`,
+      ]);
+
+    await Promise.allSettled(await deleteFiles(valuesToDelete));
+  }
+
+  imageFinder(page) {
+    const images = page.text.flatMap(({ value }) =>
+      value.match(/<img([\w\W]+?)>/g)
+    );
+    const uniqueImages = new Set([...images]);
+    return [...uniqueImages];
   }
 }
 module.exports = new BusinessTextService();
