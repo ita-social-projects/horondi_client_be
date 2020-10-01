c802f5411801931636c7be8d2aea736392c37947
Nadiya Fomenko <47325620+nadiyafomenko@users.noreply.github.com>
2020-10-01 16:09:59 +0300

    refactor material testing approach

--- __tests__/materials/material.variables.js
+++ __tests__/materials/material.variables.js
@@ -1,9 +1,16 @@
-require('dotenv').config();
+const materialDoesNotExistId = '1f2ad410eb01783384e6111b';
+
+const skip = 0;
+const wrongSkip = -5;
+const wrongLimit = -3;
+const limit = 5;
+const limitZero = 0;
 
 const user = {
-  email: process.env.ADMIN_EMAIL,
-  password: process.env.ADMIN_PASS,
+  email: process.env.SUPER_ADMIN_EMAIL,
+  password: process.env.SUPER_ADMIN_PASSWORD,
 };
+
 const languageTypeName = { __typename: 'Language' };
 const currencyTypeName = { __typename: 'CurrencySet' };
 const imageTypeName = { __typename: 'ImageSet' };
@@ -30,17 +37,17 @@ const material = {
         { lang: 'uk', value: 'Тестовий колір' },
         { lang: 'en', value: 'Test color' },
       ],
+      simpleName: [
+        { lang: 'uk', value: 'проста назва кольору' },
+        { lang: 'en', value: 'simple color name' },
+      ],
+      available: true,
       images: {
         large: 'large_test',
         medium: 'medium_test',
         small: 'small_test',
         thumbnail: 'thumbnail_test',
       },
-      available: true,
-      simpleName: [
-        { lang: 'uk', value: 'проста назва кольору' },
-        { lang: 'en', value: 'simple color name' },
-      ],
     },
   ],
 };
@@ -124,23 +131,18 @@ const materialWithExistingName = {
   ],
 };
 
-const skip = 0;
-const limit = 5;
-const limitZero = 0;
-const wrongSkip = -5;
-const wrongLimit = -3;
-
 module.exports = {
+  materialDoesNotExistId,
+  skip,
+  wrongSkip,
+  wrongLimit,
+  limit,
+  limitZero,
   user,
-  material,
-  materialToUpdate,
-  materialWithExistingName,
   languageTypeName,
   currencyTypeName,
   imageTypeName,
-  skip,
-  limit,
-  wrongSkip,
-  wrongLimit,
-  limitZero,
+  material,
+  materialToUpdate,
+  materialWithExistingName,
 };
