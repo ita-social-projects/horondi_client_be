e9098f42ffa4aa4a10ca8f9c9849be416d157290
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-10-03 22:25:38 +0200

    update: imports

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const connectDB = require('./config/db');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const permissions = require('./permissions');
-const errorOutputPlugin = require('./plugins/error-output.plugin')
+const errorOutputPlugin = require('./plugins/error-output.plugin');
 const formatError = require('./utils/format-error');
+const verifyUser = require('./utils/verify-user');
+const userService = require('./modules/user/user.service');
 require('dotenv').config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
 connectDB();
