d2b6029db4f8ab7c0e8d618eb5781ac03d9334d3
Destrobo <bobbress304@gmail.com>
2020-10-08 12:08:13 +0300

    Added db-seeder config for local database

--- import-data/index.js
+++ import-data/index.js
@@ -2,19 +2,31 @@ const path = require('path');
 process.env.DEBUG = 'mongo-seeding';
 const { Seeder } = require('mongo-seeding');
 
-// const config = {
-//   database: '',
-//   dropDatabase: false,
-// };
-
 const config = {
   database: {
     host: '127.0.0.1',
     port: 27017,
     name: 'horondi',
+    username: 'root',
+    password: 'root',
+    options: {
+      authSource: 'admin',
+      readPreference: 'primary',
+      appname: 'MongoDB',
+      ssl: false
+    }
   },
   dropDatabase: true,
 };
+
+// const config = {
+//   database: {
+//     host: '127.0.0.1',
+//     port: 27017,
+//     name: 'horondi',
+//   },
+//   dropDatabase: true,
+// };
 
 const seeder = new Seeder(config);
 const collections = seeder.readCollectionsFromPath(
