dd76adcf83c9a48babd3d7ee1543adaf7ea13336
VitalikVoicix <voicix1502@gmail.com>
2020-10-10 14:35:38 +0300

    fix next test

--- __tests__/comment/comment.mutations.test.js
+++ __tests__/comment/comment.mutations.test.js
@@ -24,7 +24,7 @@ describe('Comment queries', () => {
   beforeAll(async () => {
     operations = await setupApp();
   });
-  it('should add a new comment', async () => {
+  it('1 should add a new comment', async () => {
     const res = await operations
       .mutate({
         mutation: gql`
@@ -61,7 +61,7 @@ describe('Comment queries', () => {
     expect(receivedComment).toHaveProperty('user', newComment.user);
     expect(receivedComment).toHaveProperty('productId', newComment.productId);
   });
-  it('should return error if to add comment to not existing product', async () => {
+  it('2 should return error if to add comment to not existing product', async () => {
     const res = await operations
       .mutate({
         mutation: gql`
@@ -99,7 +99,7 @@ describe('Comment queries', () => {
     expect(receivedComment).toHaveProperty('statusCode', 404);
   });
 
-  it('should update comment', async () => {
+  it(' 3 should update comment', async () => {
     const res = await operations
       .mutate({
         mutation: gql`
@@ -138,7 +138,8 @@ describe('Comment queries', () => {
       updatedComment.productId
     );
   });
-  it('should return error if id of comment to update is not correct', async () => {
+
+  it('4 should return error if id of comment to update is not correct', async () => {
     const res = await operations
       .mutate({
         mutation: gql`
@@ -166,6 +167,71 @@ describe('Comment queries', () => {
       })
       .catch(e => e);
     const receivedComment = res.data.updateComment;
+    expect(receivedComment).not.toBeNull();
+    expect(receivedComment).toBeDefined();
+    expect(receivedComment).toHaveProperty('message', COMMENT_NOT_FOUND);
+    expect(receivedComment).toHaveProperty('statusCode', 404);
+  });
+
+  it('5 should  delete comment', async () => {
+    const res = await operations
+      .mutate({
+        mutation: gql`
+          mutation($id: ID!) {
+            deleteComment(id: $id) {
+              ... on Comment {
+                _id
+                text
+                user {
+                  email
+                }
+                product {
+                  _id
+                }
+                show
+              }
+              ... on Error {
+                message
+                statusCode
+              }
+            }
+          }
+        `,
+        variables: { id: commentId },
+      })
+      .catch(e => e);
+    const receivedComment = res.data.deleteComment;
+    expect(receivedComment).not.toBeNull();
+    expect(receivedComment).toBeDefined();
+    expect(receivedComment).toHaveProperty('text', updatedComment.text);
+    expect(receivedComment).toHaveProperty('show', updatedComment.show);
+    expect(receivedComment).toHaveProperty('user', updatedComment.user);
+    expect(receivedComment).toHaveProperty(
+      'productId',
+      updatedComment.productId
+    );
+  });
+
+  it('6 should  return error when delete not existed comment', async () => {
+    const res = await operations
+      .mutate({
+        mutation: gql`
+          mutation($id: ID!) {
+            deleteComment(id: $id) {
+              ... on Comment {
+                _id
+              }
+              ... on Error {
+                message
+                statusCode
+              }
+            }
+          }
+        `,
+        variables: { id: commentWrongId },
+      })
+      .catch(e => e);
+    const receivedComment = res.data.deleteComment;
     expect(receivedComment).not.toBeNull();
     expect(receivedComment).toBeDefined();
     expect(receivedComment).toHaveProperty('message', COMMENT_NOT_FOUND);
