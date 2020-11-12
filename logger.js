a5ae008246268ff7d1c379a3f20f1ceb38760d8b
VitalikVoicix <voicix1502@gmail.com>
2020-11-12 17:53:58 +0200

    change new file

--- logger.js
+++ logger.js
@@ -4,12 +4,20 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
+    // winston.format.json(),
     winston.format.printf(({ message, timestamp }) => {
       const winstonMessage = JSON.parse(message + '');
       return `Date=${timestamp} key=${winstonMessage.key} value=${winstonMessage.value} `;
     })
   ),
-  transports: [new winston.transports.Console()],
+
+  // defaultMeta: { service: 'user-service' },
+
+  transports: [
+    new winston.transports.Console(),
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
+  ],
 });
 
 module.exports = logger;
