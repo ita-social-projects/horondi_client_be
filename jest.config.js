c3105946da4870d0d7a0182a27bc0358289e7dde
Ivan <vaaeking@gmail.com>
2020-11-18 14:22:02 +0200

    change sonar configs

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   maxConcurrency: 1,
   testRegex: '__tests__/.*.test.js$',
   globalTeardown: './__tests__/teardown.js',
+  testResultsProcessor: 'jest-sonar-reporter',
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/'],
 };
