3ef41588827a7550e87db6f12d409649b27e3da6
Roman <romaniv788@gmail.com>
2020-12-21 19:26:44 +0200

    code refactoring

--- modules/constructor/constructor-bottom/constructor-bottom.service.js
+++ modules/constructor/constructor-bottom/constructor-bottom.service.js
@@ -1,5 +1,4 @@
 const ConstructorBottom = require('./constructor-bottom.model');
-const Currency = require('../../currency/currency.model');
 const Material = require('../../material/material.service');
 const {
   CONSTRUCTOR_BOTTOM_NOT_FOUND,
@@ -7,6 +6,7 @@ const {
   CONSTRUCTOR_BOTTOM_ALREADY_EXIST,
 } = require('../../../error-messages/constructor-bottom.messages');
 const { uploadFiles, deleteFiles } = require('../../upload/upload.service');
+const { calculatePrice } = require('../../currency/currency.utils');
 
 class ConstructorBottomService {
   async getConstructorBottomById(id) {
@@ -34,24 +34,9 @@ class ConstructorBottomService {
     const imageResults = await uploadResult[0];
     data.image = imageResults.fileNames;
 
-    data.basePrice = await this.calculatePrice(data.basePrice);
+    data.basePrice = await calculatePrice(data.basePrice);
     data.material = await Material.getMaterialById(data.material);
     return await new ConstructorBottom(data).save();
-  }
-
-  async calculatePrice(price) {
-    const { convertOptions } = await Currency.findOne();
-
-    return [
-      {
-        value: Math.round(price * convertOptions[0].exchangeRate * 100),
-        currency: 'UAH',
-      },
-      {
-        value: Math.round(price * 100),
-        currency: 'USD',
-      },
-    ];
   }
 
   async updateConstructorBottom(id, newConstructorBottom, upload) {
@@ -70,7 +55,7 @@ class ConstructorBottomService {
       const imageResults = await uploadResult[0];
       newConstructorBottom.image = imageResults.fileNames;
     }
-    newConstructorBottom.basePrice = await this.calculatePrice(
+    newConstructorBottom.basePrice = await calculatePrice(
       newConstructorBottom.basePrice
     );
     return ConstructorBottom.findByIdAndUpdate(id, newConstructorBottom, {
@@ -94,20 +79,8 @@ class ConstructorBottomService {
     return constructorBottom;
   }
 
-  async checkConstructorBottomExist(data, id) {
-    let constructorBottomCount;
-    if (id) {
-      constructorBottomCount = await ConstructorBottom.countDocuments({
-        _id: { $ne: id },
-        name: {
-          $elemMatch: {
-            $or: [{ value: data.name[0].value }, { value: data.name[1].value }],
-          },
-        },
-      });
-      return constructorBottomCount > 0;
-    }
-    constructorBottomCount = await ConstructorBottom.countDocuments({
+  async checkConstructorBottomExist(data) {
+    let constructorBottomCount = await ConstructorBottom.countDocuments({
       name: {
         $elemMatch: {
           $or: [{ value: data.name[0].value }, { value: data.name[1].value }],
