f37a35070a7a26fcb76d509eae98273e7889e6e5
Dmytro Malon <dmytro.malonn@gmail.com>
2020-12-25 20:12:10 +0200

    fix come smell

--- modules/upload/upload.utils.js
+++ modules/upload/upload.utils.js
@@ -1,7 +1,7 @@
 const uploadService = require('./upload.service');
 
 const uploadLargeImage = async file => {
-  result = await uploadService.uploadFile(file, ['large']);
+  let result = await uploadService.uploadFile(file, ['large']);
   return result.fileNames.large;
 };
 
