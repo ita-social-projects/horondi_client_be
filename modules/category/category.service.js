208d63206283d9a526472e29b3f6321fabd0885b
Roman <romaniv788@gmail.com>
2020-12-28 13:01:35 +0200

    fix burger

--- modules/category/category.service.js
+++ modules/category/category.service.js
@@ -8,6 +8,7 @@ const {
 } = require('../../error-messages/category.messages');
 const uploadService = require('../upload/upload.service');
 const { OTHERS } = require('../../consts');
+const modelService = require('../model/model.service');
 
 class CategoryService {
   async getAllCategories() {
@@ -60,26 +61,19 @@ class CategoryService {
     const categories = await this.getAllCategories();
 
     const data = categories.map(async category => {
-      const products = await Product.find({ category: category._id });
-      const uniqueModels = [];
-      const models = products
-        .map(product => ({
-          name: [...product.model],
-          _id: product._id,
-        }))
-        .filter(({ name }) => {
-          if (!uniqueModels.includes(name[0].value)) {
-            uniqueModels.push(name[0].value);
-            return true;
-          }
-          return false;
-        });
+      const models = await Model.find({ category: category._id });
+      const modelsFields = models.map(async model => {
+        return {
+          name: model.name,
+          _id: model._id,
+        };
+      });
       return {
         category: {
           name: [...category.name],
           _id: category._id,
         },
-        models,
+        models: modelsFields,
       };
     });
 
