9f9b20b20dd7413a90a50acf89d1206dfcbc82d4
Roman <romaniv788@gmail.com>
2021-01-16 20:10:32 +0200

    code refactoring

--- modules/duplication/duplication.service.js
+++ modules/duplication/duplication.service.js
@@ -6,25 +6,29 @@ class DuplicationService {
   async checkItemExist(data, type, field, id) {
     if (data[field]) {
       if (type === 'LanguageSet') {
-        return id
-          ? await this.model.countDocuments({
-              _id: { $ne: id },
-              name: {
-                $elemMatch: {
-                  $or: data[field].map(({ value }) => ({ value })),
-                },
-              },
-            })
-          : await this.model.countDocuments({
-              name: {
-                $elemMatch: {
-                  $or: data[field].map(({ value }) => ({ value })),
-                },
-              },
-            });
+        return countDuplicationLanguageSet(data, field, id);
       }
     }
     throw new Error('Please provide data with field');
+  }
+
+  async countDuplicationLanguageSet(data, field, id) {
+    return id
+      ? await this.model.countDocuments({
+          _id: { $ne: id },
+          name: {
+            $elemMatch: {
+              $or: data[field].map(({ value }) => ({ value })),
+            },
+          },
+        })
+      : await this.model.countDocuments({
+          name: {
+            $elemMatch: {
+              $or: data[field].map(({ value }) => ({ value })),
+            },
+          },
+        });
   }
 }
 
