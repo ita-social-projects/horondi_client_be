6fcd867776879fcbc5ee6db561a79daae13b44a3
Roman <59802190+moran711@users.noreply.github.com>
2021-01-17 12:12:26 +0200

    Update duplication.service.js

--- modules/duplication/duplication.service.js
+++ modules/duplication/duplication.service.js
@@ -6,7 +6,7 @@ class DuplicationService {
   async checkItemExist(data, type, field, id) {
     if (data[field]) {
       if (type === 'LanguageSet') {
-        return this.countDuplicationLanguageSet(data, field, id);
+        return await this.countDuplicationLanguageSet(data, field, id);
       }
     }
     throw new Error('Please provide data with field');
