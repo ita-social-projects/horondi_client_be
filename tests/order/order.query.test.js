d8038de25d9755ac376a9a17ee81baea6f40e877
jul4ajka <jul4ajka@gmail.com>
2021-01-26 20:44:51 +0200

    Fixed tests for sizes

--- tests/order/order.query.test.js
+++ tests/order/order.query.test.js
@@ -42,7 +42,7 @@ const { setupApp } = require('../helper-functions');
 
 jest.mock('../../modules/upload/upload.service');
 jest.mock('../../modules/currency/currency.model.js');
-c;
+jest.mock('../../modules/currency/currency.utils.js');
 jest.mock('../../modules/product/product.utils.js');
 
 let colorId;
