de5c3ac997bc337d773592391c99885dce483001
M1ndForce <66359796+M1ndForce@users.noreply.github.com>
2021-01-26 10:22:39 +0200

    fix mistakes

--- tests/order/order.query.test.js
+++ tests/order/order.query.test.js
@@ -42,9 +42,7 @@ const { setupApp } = require('../helper-functions');
 
 jest.mock('../../modules/upload/upload.service');
 jest.mock('../../modules/currency/currency.model.js');
-jest.mock('../../modules/delivery/nova-poshta/nova-poshta.service.js');
 jest.mock('../../modules/currency/currency.utils.js');
-jest.mock('../../modules/delivery/delivery.service.js');
 jest.mock('../../modules/product/product.utils.js');
 
 let colorId;
