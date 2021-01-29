740d9195528da0801639f37834d0647c58da21de
Roman <romaniv788@gmail.com>
2021-01-29 20:28:07 +0200

    fix duplication

--- modules/category/category.graphql.js
+++ modules/category/category.graphql.js
@@ -1,17 +1,16 @@
-const categoryType = `
-  type Category {
+const sharedFields = `
   _id: ID!
   code: String!
   name: [Language]
   images: ImageSet
   available: Boolean
-}
+`;
+const categoryType = `
+  type Category {
+    ${sharedFields}
+  }
   type CategoryWithModels {
-    _id: ID!
-    code: String!
-    name: [Language]
-    images: ImageSet
-    available: Boolean
+    ${sharedFields}
     models: [Model]
   }
 `;
