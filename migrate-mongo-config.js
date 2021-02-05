528fddfd9d651bc15237e84a5ffdc998c579e0b3
Roman <romaniv788@gmail.com>
2021-02-05 17:36:53 +0200

    small fixes

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,30 +1,18 @@
-// In this file you can configure migrate-mongo
+const { MONGO_URL } = require('../dotenvValidator');
 
 const config = {
   mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
-
-    // TODO Change this to your database name:
-    databaseName: "test1",
+    url: MONGO_URL,
+    databaseName: 'horondi',
 
     options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
-      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
-      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    }
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
   },
-
-  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: "migrations",
-
-  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
-  changelogCollectionName: "changelog",
-
-  // The file extension to create migrations and search for in migration dir 
-  migrationFileExtension: ".js"
+  migrationsDir: 'migrations',
+  changelogCollectionName: 'changelog',
+  migrationFileExtension: '.js',
 };
 
-// Return the config as a promise
 module.exports = config;
