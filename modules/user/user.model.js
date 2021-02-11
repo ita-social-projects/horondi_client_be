a5bf375d2fc4482e6a4cba0061f8b5c7b77a37cb
Liubomyr <louidropson@gmail.com>
2021-02-11 18:10:00 +0200

    commit

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -9,7 +9,7 @@ const {
 } = require('../../error-messages/user.messages');
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
+  firstName: { type: String },
   lastName: String,
   role: {
     type: String,
