32a6010e95c93c7ef0579329fe15110f605a6e40
Roman <romaniv788@gmail.com>
2021-02-15 19:09:53 +0200

    implement migration

---
+++ consts/migrations.js
@@ -0,0 +1,16 @@
+const sizeName = {
+  name: [
+    {
+      lang: 'ua',
+      value: 'Назва розміру',
+    },
+    {
+      lang: 'en',
+      value: 'Sizes name',
+    },
+  ],
+};
+
+module.exports = {
+  sizeName,
+};
