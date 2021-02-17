95f7667966fd4ad0913a3a0008a5df6a48bf2ded
Kiyooooko <olena.shchadylo@ukr.net>
2021-02-17 14:59:43 +0200

    fix service for comment searching

--- typeDefs.js
+++ typeDefs.js
@@ -402,9 +402,7 @@ const typeDefs = gql`
     getPopularProducts: StatisticBar!
     getAllComments(
       filter: FilterInputComponent
-      skip: Int
-      limit: Int
-      sort: SortInputComponent
+      pagination: Pagination
     ): PaginatedComments!
     getCommentById(id: ID!): CommentResult
     getAllCommentsByProduct(
