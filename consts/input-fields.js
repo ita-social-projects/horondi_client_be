638be970b63958dc2200079b2ac269acdcde5ca0
Yuliia Kucher <yuliia.s.kucher@gmail.com>
2021-03-21 21:44:09 +0200

    Merge branch 'development' of https://github.com/ita-social-projects/horondi_client_be into feature/564
    # Conflicts:
    #	consts/input-fields.js

--- consts/input-fields.js
+++ consts/input-fields.js
@@ -1,6 +1,11 @@
 const INPUT_FIELDS = {
   USER: 'user',
+  QUANTITY: 'quantity',
   ORDER: 'order',
+  LIMIT: 'limit',
+  SKIP: 'skip',
+  FILTER: 'filter',
+  DATE: 'date',
 };
 
 module.exports = {
