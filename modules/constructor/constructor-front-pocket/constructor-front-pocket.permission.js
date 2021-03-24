0785525803c1fbe70031fd748afcaf10df1509a4
Вадим Ревула <vadimrevula@MacBook-Pro-Vadim.local>
2021-03-24 18:51:47 +0200

    fixed bug

--- modules/constructor/constructor-front-pocket/constructor-front-pocket.permission.js
+++ modules/constructor/constructor-front-pocket/constructor-front-pocket.permission.js
@@ -1,4 +1,4 @@
-const { allow } = require('graphql-shield');
+const { and, allow } = require('graphql-shield');
 const { hasRoles } = require('../../../utils/rules');
 const { roles } = require('../../../consts');
 const { ADMIN, SUPERADMIN } = roles;
