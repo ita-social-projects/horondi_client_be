5332b2593e9e330443c58a26e2ba6f2faeb947d2
Yuliia Kucher <yuliia.s.kucher@gmail.com>
2021-03-25 12:58:50 +0200

    fixed regexp

--- consts/regexp.js
+++ consts/regexp.js
@@ -2,7 +2,7 @@ const emailRegExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 const numberRegExp = new RegExp(/^\+?3?8?(0\d{9})$/);
 const uaLanguageRegExp = new RegExp(/^[а-яїієґ0-9\s]+$/i);
 const enLanguageRegExp = new RegExp(/^[a-z0-9\s]+$/i);
-const userNameRegExp = /^(?=.{2,30}$)[a-zA-Zа-яА-ЯІіїє]+(([',. -][a-zA-Zа-яА-ЯІіїє])?[a-zA-Zа-яА-ЯІіїє]*)*$/u;
+const userNameRegExp = /^(?=.{2,30}$)[a-zA-Zа-яА-ЯІЄіїє]+(([',. -][a-zA-Zа-яА-ЯІЄіїє])?[a-zA-Zа-яА-ЯІЄіїє]*)*$/u;
 const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,30}$/;
 
 module.exports = {
