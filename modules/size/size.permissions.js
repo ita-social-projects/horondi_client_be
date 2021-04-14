b00b49ea46a17606a8686a65b0130de7f4fc3b6e
ostgit12 <zostap200@gmail.com>
2021-04-14 18:47:31 +0300

    add Joi validation for size service methods

--- modules/size/size.permissions.js
+++ modules/size/size.permissions.js
@@ -1,16 +1,28 @@
-const { allow } = require('graphql-shield');
+const { allow, and } = require('graphql-shield');
 const { hasRoles } = require('../../utils/rules');
 const { roles } = require('../../consts');
-
 const { ADMIN, SUPERADMIN } = roles;
+const { sizeInputValidator } = require('../../validators/size.validator');
+const { inputDataValidation } = require('../../utils/rules');
+
+const {
+  DB_COLLECTIONS_NAMES: { SIZE },
+} = require('../../consts/db-collections-names');
+
 const sizePermissionsQuery = {
   getAllSizes: allow,
   getSizeById: allow,
 };
 const sizePermissionsMutations = {
-  addSize: hasRoles([ADMIN, SUPERADMIN]),
+  addSize: and(
+    inputDataValidation(SIZE, sizeInputValidator),
+    hasRoles([ADMIN, SUPERADMIN])
+  ),
   deleteSize: hasRoles([ADMIN, SUPERADMIN]),
-  updateSize: hasRoles([ADMIN, SUPERADMIN]),
+  updateSize: and(
+    inputDataValidation(SIZE, sizeInputValidator),
+    hasRoles([ADMIN, SUPERADMIN])
+  ),
 };
 
 module.exports = {
---
+++ validators/size.validator.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+
+const sizeInputValidator = Joi.object({
+  name: Joi.string()
+    .min(1)
+    .max(2)
+    .required(),
+  simpleName: Joi.array().items({
+    lang: Joi.string()
+      .trim()
+      .required(),
+    value: Joi.string()
+      .trim()
+      .required(),
+  }),
+  heightInCm: Joi.number()
+    .integer()
+    .min(1)
+    .max(35)
+    .required(),
+  widthInCm: Joi.number()
+    .integer()
+    .min(1)
+    .max(35)
+    .required(),
+  depthInCm: Joi.number()
+    .integer()
+    .min(1)
+    .max(35)
+    .required(),
+  volumeInLiters: Joi.number()
+    .integer()
+    .min(1)
+    .max(35)
+    .required(),
+  weightInKg: Joi.number()
+    .min(0.1)
+    .max(5)
+    .required(),
+  available: Joi.boolean().required(),
+  additionalPrice: Joi.number()
+    .integer()
+    .required(),
+});
+
+module.exports = {
+  sizeInputValidator,
+};
