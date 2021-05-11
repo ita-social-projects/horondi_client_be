19e748f9676700c7513f53cdcefffe438791f543
AndriyLymych <lymych1994@gmail.com>
2021-05-11 18:35:46 +0300

    fix category filters

--- modules/comment/comment.service.js
+++ modules/comment/comment.service.js
@@ -2,152 +2,152 @@ const Comment = require('./comment.model');
 const RuleError = require('../../errors/rule.error');
 const Product = require('../product/product.model');
 const {
-    STATUS_CODES: {NOT_FOUND},
+  STATUS_CODES: { NOT_FOUND },
 } = require('../../consts/status-codes');
 const {
-    COMMENT_NOT_FOUND,
-    COMMENTS_NOT_FOUND,
-    COMMENT_FOR_NOT_EXISTING_PRODUCT,
-    COMMENT_FOR_NOT_EXISTING_USER,
-    RATE_FOR_NOT_EXISTING_PRODUCT,
+  COMMENT_NOT_FOUND,
+  COMMENTS_NOT_FOUND,
+  COMMENT_FOR_NOT_EXISTING_PRODUCT,
+  COMMENT_FOR_NOT_EXISTING_USER,
+  RATE_FOR_NOT_EXISTING_PRODUCT,
 } = require('../../error-messages/comment.messages');
 
 class CommentsService {
-    async getAllComments({filter, pagination: {skip, limit}}) {
-        const filterOptions = {};
+  async getAllComments({ filter, pagination: { skip, limit } }) {
+    const filterOptions = {};
 
-        if (filter?.show?.length) {
-            filterOptions.show = {$in: filter.show};
-        }
-
-        if (filter?.date?.dateFrom) {
-            filterOptions.date = {
-                $gte: filter.date.dateFrom.toString(),
-            };
-        }
-        if (filter?.date?.dateTo) {
-            filterOptions.date = {
-                $lte: filter.date.dateTo.toString(),
-            };
-        }
-
-        if (filter?.search) {
-            const search = filter.search.trim();
-            filterOptions.text = {$regex: `${search}`, $options: 'i'}
-        }
-
-        const items = await Comment.find(filterOptions)
-            .limit(limit)
-            .skip(skip)
-            .exec();
-
-        const count = await Comment.find(filterOptions).countDocuments();
-
-        return {
-            items,
-            count
-        };
+    if (filter?.show?.length) {
+      filterOptions.show = { $in: filter.show };
     }
 
-    async getCommentById(id) {
-        const comment = await Comment.findById(id).exec();
-        if (!comment) {
-            throw new Error(COMMENT_NOT_FOUND);
-        }
-        return comment;
+    if (filter?.date?.dateFrom) {
+      filterOptions.date = {
+        $gte: filter.date.dateFrom.toString(),
+      };
+    }
+    if (filter?.date?.dateTo) {
+      filterOptions.date = {
+        $lte: filter.date.dateTo.toString(),
+      };
     }
 
-    async getRecentComments(limit) {
-        const comments = await Comment.find()
-            .sort({date: -1})
-            .limit(limit)
-            .exec();
-        if (!comments?.length) {
-            throw new RuleError(COMMENTS_NOT_FOUND, NOT_FOUND);
-        }
-        return comments;
+    if (filter?.search) {
+      const search = filter.search.trim();
+      filterOptions.text = { $regex: `${search}`, $options: 'i' };
     }
 
-    async getAllCommentsByProduct({productId}) {
-        const product = await Product.findById(productId).exec();
-        if (!product) {
-            throw new Error(COMMENT_NOT_FOUND);
-        }
-        const comments = await Comment.find({product: productId}).exec();
-        return comments;
+    const items = await Comment.find(filterOptions)
+      .limit(limit)
+      .skip(skip)
+      .exec();
+
+    const count = Comment.find(filterOptions).countDocuments();
+
+    return {
+      items,
+      count,
+    };
+  }
+
+  async getCommentById(id) {
+    const comment = await Comment.findById(id).exec();
+    if (!comment) {
+      throw new Error(COMMENT_NOT_FOUND);
     }
+    return comment;
+  }
 
-    async getAllCommentsByUser(userId) {
-        const comments = await Comment.find({user: userId}).exec();
-        if (!comments.length) {
-            throw new Error(COMMENT_FOR_NOT_EXISTING_USER);
-        }
-        return comments;
+  async getRecentComments(limit) {
+    const comments = await Comment.find()
+      .sort({ date: -1 })
+      .limit(limit)
+      .exec();
+    if (!comments?.length) {
+      throw new RuleError(COMMENTS_NOT_FOUND, NOT_FOUND);
     }
+    return comments;
+  }
 
-    async updateComment(id, comment) {
-        const updatedComment = await Comment.findByIdAndUpdate(id, comment, {
-            new: true,
-        }).exec();
-        if (!updatedComment) {
-            throw new Error(COMMENT_NOT_FOUND);
-        }
-        return updatedComment;
+  async getAllCommentsByProduct({ productId }) {
+    const product = await Product.findById(productId).exec();
+    if (!product) {
+      throw new Error(COMMENT_NOT_FOUND);
     }
+    const comments = await Comment.find({ product: productId }).exec();
+    return comments;
+  }
 
-    async addComment(id, data) {
-        const product = await Product.findById(id).exec();
-        if (!product) {
-            throw new Error(COMMENT_FOR_NOT_EXISTING_PRODUCT);
-        }
-        return new Comment(data).save();
+  async getAllCommentsByUser(userId) {
+    const comments = await Comment.find({ user: userId }).exec();
+    if (!comments.length) {
+      throw new Error(COMMENT_FOR_NOT_EXISTING_USER);
     }
+    return comments;
+  }
 
-    async deleteComment(id) {
-        const deletedComment = await Comment.findByIdAndDelete(id).exec();
-        if (!deletedComment) {
-            throw new Error(COMMENT_NOT_FOUND);
-        }
-        return deletedComment;
+  async updateComment(id, comment) {
+    const updatedComment = await Comment.findByIdAndUpdate(id, comment, {
+      new: true,
+    }).exec();
+    if (!updatedComment) {
+      throw new Error(COMMENT_NOT_FOUND);
     }
+    return updatedComment;
+  }
 
-    async addRate(id, data, user) {
-        const product = await Product.findById(id).exec();
-
-        if (!product) {
-            throw new Error(RATE_FOR_NOT_EXISTING_PRODUCT);
-        }
-        const {userRates} = product;
-        let {rateCount} = product;
-
-        const {rate} =
-        userRates.find(rating => String(rating.user) === String(user._id)) || {};
-
-        const rateSum = product.rate * rateCount - (rate || !!rate) + data.rate;
-        if (!rate) {
-            rateCount++;
-        }
-        const newRate = rateSum / rateCount;
-
-        const newUserRates = rate
-            ? userRates.map(item =>
-                String(item.user) === String(user._id)
-                    ? {user: item.user, rate: data.rate}
-                    : item
-            )
-            : [...userRates, {...data, user: user._id}];
-
-        const rateToAdd = await Product.findByIdAndUpdate(
-            id,
-            {
-                rateCount,
-                rate: newRate.toFixed(1),
-                userRates: newUserRates,
-            },
-            {new: true}
-        ).exec();
-        return rateToAdd;
+  async addComment(id, data) {
+    const product = await Product.findById(id).exec();
+    if (!product) {
+      throw new Error(COMMENT_FOR_NOT_EXISTING_PRODUCT);
     }
+    return new Comment(data).save();
+  }
+
+  async deleteComment(id) {
+    const deletedComment = await Comment.findByIdAndDelete(id).exec();
+    if (!deletedComment) {
+      throw new Error(COMMENT_NOT_FOUND);
+    }
+    return deletedComment;
+  }
+
+  async addRate(id, data, user) {
+    const product = await Product.findById(id).exec();
+
+    if (!product) {
+      throw new Error(RATE_FOR_NOT_EXISTING_PRODUCT);
+    }
+    const { userRates } = product;
+    let { rateCount } = product;
+
+    const { rate } =
+      userRates.find(rating => String(rating.user) === String(user._id)) || {};
+
+    const rateSum = product.rate * rateCount - (rate || !!rate) + data.rate;
+    if (!rate) {
+      rateCount++;
+    }
+    const newRate = rateSum / rateCount;
+
+    const newUserRates = rate
+      ? userRates.map(item =>
+          String(item.user) === String(user._id)
+            ? { user: item.user, rate: data.rate }
+            : item
+        )
+      : [...userRates, { ...data, user: user._id }];
+
+    const rateToAdd = await Product.findByIdAndUpdate(
+      id,
+      {
+        rateCount,
+        rate: newRate.toFixed(1),
+        userRates: newUserRates,
+      },
+      { new: true }
+    ).exec();
+    return rateToAdd;
+  }
 }
 
 module.exports = new CommentsService();
