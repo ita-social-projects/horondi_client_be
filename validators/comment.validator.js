5d288121e48396460907a2fdfb87817d69eab0bd
AndriyLymych <lymych1994@gmail.com>
2021-05-19 15:21:22 +0300

    add comment owner

--- validators/comment.validator.js
+++ validators/comment.validator.js
@@ -5,6 +5,10 @@ const replyCommentValidator = Joi.object({
     .min(2)
     .max(1000),
   refToReplyComment: Joi.string(),
+  answererEmail: Joi.string().email(),
+  answererName: Joi.string()
+    .min(2)
+    .max(20),
   answerer: Joi.string(),
   showReplyComment: Joi.boolean(),
 });
