22fefefb2cf63db8d71e40ae948d2a96650b4767
Renfort <ivan1999143@gmail.com>
2021-05-24 01:20:30 +0300

    fixed checks

--- validators/home-page-images.validator.js
+++ validators/home-page-images.validator.js
@@ -1,6 +1,9 @@
 const Joi = require('joi');
 
 const homePageImagesValidator = Joi.object({
+  id: string()
+    .trim()
+    .required(),
   images: Joi.object({
     large: Joi.string().trim(),
     medium: Joi.string().trim(),
