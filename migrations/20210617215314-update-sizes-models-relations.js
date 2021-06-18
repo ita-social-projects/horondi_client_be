bd8f73486381da3b375509eccb854ded7aca44cb
Ivan <romanomess@gmail.com>
2021-06-18 23:57:09 +0300

    updated migration

--- migrations/20210617215314-update-sizes-models-relations.js
+++ migrations/20210617215314-update-sizes-models-relations.js
@@ -6,63 +6,92 @@ module.exports = {
       .collection('sizes')
       .updateMany({}, { $unset: { simpleName: 1 } }, false, true);
     await db.collection('sizes').updateOne(
+      // L => Rolltop
       { _id: ObjectId('604394a2a7532c33dcb326d5') },
       {
         $set: {
-          modelId: [
-            { _id: ObjectId('6043bf9e3e06ad3edcdb7b30') },
-            { _id: ObjectId('6043c1223e06ad3edcdb7b31') },
-            { _id: ObjectId('6043c1983e06ad3edcdb7b32') },
-            { _id: ObjectId('60ba9acbba1b8596281cea18') },
-          ],
+          modelId: ObjectId('6043bf9e3e06ad3edcdb7b30'),
         },
       }
     );
+    // M => Rolltop
     await db.collection('sizes').updateOne(
       { _id: ObjectId('604394cba7532c33dcb326d6') },
       {
         $set: {
-          modelId: [
-            { _id: ObjectId('6043bf9e3e06ad3edcdb7b30') },
-            { _id: ObjectId('6043c1223e06ad3edcdb7b31') },
-            { _id: ObjectId('60ba9acbba1b8596281cea18') },
-          ],
+          modelId: ObjectId('6043bf9e3e06ad3edcdb7b30'),
         },
       }
     );
     await db.collection('sizes').updateOne(
+      // S => Rolltop
       { _id: ObjectId('60439516a7532c33dcb326d7') },
       {
         $set: {
-          modelId: [
-            { _id: ObjectId('6043bf9e3e06ad3edcdb7b30') },
-            { _id: ObjectId('6043c1223e06ad3edcdb7b31') },
-            { _id: ObjectId('60ba9acbba1b8596281cea18') },
-          ],
+          modelId: ObjectId('6043bf9e3e06ad3edcdb7b30'),
         },
       }
     );
-    await db.collection('sizes').updateOne(
-      { _id: ObjectId('604395c9a7532c33dcb326d9') },
-      {
-        $set: {
-          modelId: [{ _id: ObjectId('60467dfd873045422c1dbf90') }],
-        },
-      }
-    );
+    // M => Wallet
     await db.collection('sizes').updateOne(
       { _id: ObjectId('604395a8a7532c33dcb326d8') },
       {
         $set: {
-          modelId: [{ _id: ObjectId('60467dfd873045422c1dbf90') }],
+          modelId: ObjectId('60467dfd873045422c1dbf90'),
         },
       }
     );
+    // L => New
     await db.collection('sizes').updateOne(
-      { _id: ObjectId('60467e30873045422c1dbf91') },
+      { _id: ObjectId('604395c9a7532c33dcb326d9') },
       {
         $set: {
-          modelId: [{ _id: ObjectId('60467f00873045422c1dbf92') }],
+          modelId: ObjectId('6043c1983e06ad3edcdb7b32'),
+        },
+      }
+    );
+    // M => New
+    await db.collection('sizes').updateOne(
+      { _id: ObjectId('60439642a7532c33dcb326da') },
+      {
+        $set: {
+          modelId: ObjectId('6043c1983e06ad3edcdb7b32'),
+        },
+      }
+    );
+    // L => Garbuz
+    await db.collection('sizes').updateOne(
+      { _id: ObjectId('6043966ea7532c33dcb326db') },
+      {
+        $set: {
+          modelId: ObjectId('6043c1223e06ad3edcdb7b31'),
+        },
+      }
+    );
+    // M => Garbuz
+    await db.collection('sizes').updateOne(
+      { _id: ObjectId('60467d0b873045422c1dbf8f') },
+      {
+        $set: {
+          modelId: ObjectId('6043c1223e06ad3edcdb7b31'),
+        },
+      }
+    );
+    // S => Bananka
+    await db.collection('sizes').updateOne(
+      { _id: ObjectId('60467f00873045422c1dbf92') },
+      {
+        $set: {
+          modelId: ObjectId('60467f00873045422c1dbf92'),
+        },
+      }
+    );
+    // M => Symka
+    await db.collection('sizes').updateOne(
+      { _id: ObjectId('604787abfc3c0b3b34fd485a') },
+      {
+        $set: {
+          modelId: ObjectId('60ba9acbba1b8596281cea18'),
         },
       }
     );
