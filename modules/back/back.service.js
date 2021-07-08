d8b4c9f762c694cf22a9296999f956b7a456e569
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-07-08 17:34:09 +0300

    fixed download photo

--- modules/back/back.service.js
+++ modules/back/back.service.js
@@ -6,9 +6,9 @@ const {
 } = require('../../utils/constructorOptionCommonFilters');
 const RuleError = require('../../errors/rule.error');
 const { BACK_NOT_FOUND } = require('../../consts/back-messages');
-const {
-  FILE_SIZES: { SMALL },
-} = require('../../consts/file-sizes');
+// const {
+//   FILE_SIZES: { SMALL },
+// } = require('../../consts/file-sizes');
 const {
   STATUS_CODES: { NOT_FOUND },
 } = require('../../consts/status-codes');
@@ -88,10 +88,17 @@ class BackService {
     }
 
     if (image) {
-      const uploadImage = await uploadService.uploadFile(image, [SMALL]);
-      back.image = uploadImage.fileNames.small;
-    }
+      if (backToUpdate.images) {
+        const images = Object.values(backToUpdate.images).filter(
+          item => typeof item === 'string' && item
+        );
+        await uploadService.deleteFiles(images);
+      }
 
+      const uploadImage = await uploadService.uploadFiles([image]);
+      const imageResults = await uploadImage[0];
+      back.images = imageResults.fileNames;
+    }
     if (back?.additionalPrice) {
       back.additionalPrice = await calculatePrice(back.additionalPrice);
     }
@@ -154,8 +161,8 @@ class BackService {
 
   async addBack(back, image, { _id: adminId }) {
     if (image) {
-      const uploadImage = await uploadService.uploadSmallImage(image);
-      back.image = uploadImage.fileNames.small;
+      const uploadImage = await uploadService.uploadFile(image);
+      back.images = uploadImage.fileNames;
     }
 
     if (back.additionalPrice) {
