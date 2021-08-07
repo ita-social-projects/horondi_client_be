35f3ac2241fa8d015c0583e287cd4a1b87e85f07
halaburdataras <Taras.Galaburda@outlook.com>
2021-08-07 03:00:33 +0300

    fixed codesmells

--- modules/bottom/bottom.resolver.js
+++ modules/bottom/bottom.resolver.js
@@ -4,7 +4,7 @@ const RuleError = require('../../errors/rule.error');
 
 const bottomQuery = {
   getAllBottoms: async (_, { limit, skip, filter }) =>
-    await bottomService.getAllBottoms(limit, skip, filter),
+    bottomService.getAllBottoms(limit, skip, filter),
   getBottomById: async (_, { id }) => {
     try {
       return await bottomService.getBottomById(id);
@@ -16,7 +16,7 @@ const bottomQuery = {
 
 const bottomMutation = {
   addBottom: async (_, { bottom, image }, { user }) =>
-    await bottomService.addBottom(bottom, image, user),
+    bottomService.addBottom(bottom, image, user),
   updateBottom: async (_, { id, bottom, image }, { user }) => {
     try {
       return await bottomService.updateBottom(id, bottom, image, user);
