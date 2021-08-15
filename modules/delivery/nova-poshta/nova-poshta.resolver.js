f45983baeaefdfeb301814e5d147f85293d848c7
Renfort <ivan1999143@gmail.com>
2021-08-15 11:43:28 +0300

    resolved conflicts

--- modules/delivery/nova-poshta/nova-poshta.resolver.js
+++ modules/delivery/nova-poshta/nova-poshta.resolver.js
@@ -1,38 +1,17 @@
-const RuleError = require('../../../errors/rule.error');
 const deliveryService = require('./nova-poshta.service');
 
 const novaPoshtaQuery = {
-  getNovaPoshtaCities: async (parent, args) => {
-    try {
-      return deliveryService.getNovaPoshtaCities(args.city);
-    } catch (e) {
-      return new RuleError(e.message, e.statusCode);
-    }
-  },
+  getNovaPoshtaCities: async (parent, args) =>
+    deliveryService.getNovaPoshtaCities(args.city),
 
-  getNovaPoshtaStreets: async (parent, args) => {
-    try {
-      return deliveryService.getNovaPoshtaStreets(args.cityRef, args.street);
-    } catch (e) {
-      return new RuleError(e.message, e.statusCode);
-    }
-  },
+  getNovaPoshtaStreets: async (parent, args) =>
+    deliveryService.getNovaPoshtaStreets(args.cityRef, args.street),
 
-  getNovaPoshtaWarehouses: async (parent, args) => {
-    try {
-      return deliveryService.getNovaPoshtaWarehouses(args.city);
-    } catch (e) {
-      return new RuleError(e.message, e.statusCode);
-    }
-  },
+  getNovaPoshtaWarehouses: async (parent, args) =>
+    deliveryService.getNovaPoshtaWarehouses(args.city),
 
-  getNovaPoshtaPrices: async (parent, args) => {
-    try {
-      return deliveryService.getNovaPoshtaPrices(args.data);
-    } catch (e) {
-      return new RuleError(e.message, e.statusCode);
-    }
-  },
+  getNovaPoshtaPrices: async (parent, args) =>
+    deliveryService.getNovaPoshtaPrices(args.data),
 };
 
 module.exports = { novaPoshtaQuery };
