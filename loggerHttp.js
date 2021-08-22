d6504961aa166cf4dbfb78d5c6acf47df46958a8
wasya1212 <wasya1212cool@gmail.com>
2021-08-22 22:37:06 +0300

    added singletone db to logger

--- loggerHttp.js
+++ loggerHttp.js
@@ -1,29 +1,28 @@
 const winston = require('winston');
-const { MONGO_URL } = require('./dotenvValidator');
 
 require('winston-mongodb');
 
 const OneWeekInSeconds = 604800;
 
-const mongoLogOptions = {
-  name: 'log_info',
-  storeHost: true,
-  db: MONGO_URL,
-  expire: Math.floor(OneWeekInSeconds / 7), // 1 day
-  collection: 'logs',
-  capped: true,
+module.exports = mongo => {
+  const mongoLogOptions = {
+    name: 'log_info',
+    storeHost: true,
+    db: mongo,
+    expire: Math.floor(OneWeekInSeconds / 7), // 1 day
+    collection: 'logs',
+    capped: true,
+  };
+
+  return winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+      winston.format.metadata()
+    ),
+
+    defaultMeta: { service: 'user-service' },
+    transports: [new winston.transports.MongoDB(mongoLogOptions)],
+  });
 };
-
-const loggerHttp = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json(),
-    winston.format.metadata()
-  ),
-
-  defaultMeta: { service: 'user-service' },
-  transports: [new winston.transports.MongoDB(mongoLogOptions)],
-});
-
-module.exports = loggerHttp;
