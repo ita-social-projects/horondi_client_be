473bf90dd4ba7037869e04839da4fc21c42d4784
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-08-29 18:37:01 +0300

    created pocket variables

---
+++ tests/pocket/pocket.variables.js
@@ -0,0 +1,40 @@
+const newPocketInputData = () => ({
+  name: [
+    { lang: 'ua', value: 'ліва' },
+    { lang: 'en', value: 'left' },
+  ],
+  optionType: 'POCKET',
+  restriction: false,
+  additionalPrice: 5,
+  positions: ['60fff9f63affc3410c21ab54'],
+});
+
+const newPocketInputDataUpdate = () => ({
+  name: [
+    { lang: 'ua', value: 'ліва' },
+    { lang: 'en', value: 'left' },
+  ],
+  optionType: 'POCKET',
+  restriction: false,
+  additionalPrice: 5,
+  positions: ['60fff9f63affc3410c21ab54'],
+});
+
+const wrongId = '5fb412d8663cf10bec9faa1a';
+
+const filter = {
+  name: '',
+  search: '',
+};
+
+const skip = 0;
+const limit = 5;
+
+module.exports = {
+  skip,
+  limit,
+  filter,
+  newPocketInputData,
+  newPocketInputDataUpdate,
+  wrongId,
+};
