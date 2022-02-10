b87c656cbed28497e6c2635c534b572b6a33c538
boris-giga <boris.ko.uni@gmail.com>
2022-02-10 17:24:00 +0200

    fixed regExp for phoneNumber

--- consts/regexp.js
+++ consts/regexp.js
@@ -1,5 +1,5 @@
 const emailRegExp = new RegExp(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/i);
-const numberRegExp = new RegExp(/^[+]*[(]?\d{1,4}[)]?[-\s/0-9]{9}$/);
+const numberRegExp = new RegExp(/^\d{9}/);
 const uaLanguageRegExp = new RegExp(/^[а-яїієґ0-9\s]+$/i);
 const enLanguageRegExp = new RegExp(/^[a-z0-9\s]+$/i);
 const userNameRegExp = /^(?=.{2,30}$)[a-zA-Zа-яА-ЯІЄЇіїє]+(([',. -][a-zA-Zа-яА-ЯІЄЇіїє])?[a-zA-Zа-яА-ЯІЄЇіїє])*$/u;
