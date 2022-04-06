dde78ccfdb8cc8f80c2d84c0079ec61a2a6d867c
Andrii Syrotenko <andruhe@gmail.com>
2022-04-06 22:24:59 +0300

    fixed: convert string to date object (#1178)

--- utils/modify-date.js
+++ utils/modify-date.js
@@ -1,4 +1,8 @@
 const modifyDate = ({ days = 0, months = 0, years = 0, date = new Date() }) => {
+  if (!(date instanceof Date)) {
+    date = new Date(date);
+  }
+
   date.setHours(0, 0, 0, 0);
   date.setDate(date.getDate() + days);
   date.setMonth(date.getMonth() + months);
