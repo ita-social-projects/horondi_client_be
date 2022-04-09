3fd18471e55657bb22b656a5229928f01e9a8776
Liubomyr <80056472+LubomyrKashuba@users.noreply.github.com>
2022-04-09 21:43:48 +0300

    order price refactor (#1190)

--- modules/order/order.graphql.js
+++ modules/order/order.graphql.js
@@ -151,11 +151,9 @@ input OrderItemInput {
   constructorBottom: ID
   constructorFrontPocket: ID
   constructorPattern: ID
-  actualPrice: Int
   quantity: Int!
   isFromConstructor: Boolean
   options: ItemOptionsInput
-  fixedPrice: Int
   price: Int
 }
 input ItemOptionsInput{
