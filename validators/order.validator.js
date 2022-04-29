d775ef338f05beadd4b40b782785b7819331263f
Anatoliy Gargash <kabantolyan@gmail.com>
2022-04-29 17:13:28 +0300

    update validator

--- validators/order.validator.js
+++ validators/order.validator.js
@@ -185,13 +185,34 @@ const nestedDeliveryValidator = Joi.object({
     currency: Joi.string().trim().required(),
     value: Joi.number().required(),
   }),
-  messenger: Joi.string().allow(''),
-  messengerPhone: Joi.string().allow(''),
-  worldWideCountry: Joi.string().allow(''),
-  stateOrProvince: Joi.string().allow(''),
-  worldWideCity: Joi.string().allow(''),
-  worldWideStreet: Joi.string().allow(''),
-  cityCode: Joi.string().allow(''),
+  messenger: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
+  messengerPhone: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
+  worldWideCountry: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
+  stateOrProvince: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().allow(null, '').optional(),
+  }),
+  worldWideCity: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
+  worldWideStreet: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
+  cityCode: Joi.string().when(SENT_BY, {
+    is: WORLDWIDE,
+    then: Joi.string().required(),
+  }),
 });
 
 const orderValidator = Joi.object({
