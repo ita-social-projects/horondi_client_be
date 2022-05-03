80046d1fb22226dba6dca928f015567b775e878a
Kharko7 <hanley77@ukr.net>
2022-05-03 13:47:05 +0300

    fixed RegExp phone number

--- consts/regexp.js
+++ consts/regexp.js
@@ -1,7 +1,7 @@
 const emailRegExp = new RegExp(
   '^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$'
 );
-const numberRegExp = new RegExp(/^\d{9}/);
+const numberRegExp = new RegExp(/^(\+380|0)\d{9}$/);
 const uaLanguageRegExp = new RegExp(/^[а-яїієґ0-9\s]+$/i);
 const enLanguageRegExp = new RegExp(/^[a-z0-9\s]+$/i);
 const userNameRegExp =
