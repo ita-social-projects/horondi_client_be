ff19d2952ff60af1899b2e485dfa3c4f6bc797ec
Evhenij Pipa <zhuvchuk1@gmail.com>
2022-08-04 10:58:35 +0300

    codesmell

--- modules/strap/strap.model.js
+++ modules/strap/strap.model.js
@@ -8,25 +8,25 @@ const {
 
 const strapSchema = new mongoose.Schema({
   name: [Language],
-  optionType: String,
   features: {
-    material: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: MATERIAL,
-    },
     color: {
       type: mongoose.Schema.Types.ObjectId,
       ref: COLOR,
     },
+    material: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: MATERIAL,
+    },
   },
   images: ImageSet,
-  absolutePrice: { type: Number, default: null },
   relativePrice: { type: Number, default: null },
+  absolutePrice: { type: Number, default: null },
   available: Boolean,
   translationsKey: {
     type: mongoose.Schema.Types.ObjectId,
     ref: TRANSLATIONS,
   },
+  optionType: String,
 });
 
 module.exports = mongoose.model(STRAP, strapSchema);
