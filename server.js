8d144f8546bdf0d84dbc96d81076e82f49a9badd
roman peretiatko <romansperetiatko@gmail.com>
2022-12-22 20:37:17 +0200

    added custom memory limit for json(express)

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
 app.use(
   cors({
     origin: [
